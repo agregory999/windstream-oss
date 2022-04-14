91cd93788b409031c68e6f6152eb3d480e1c73be
orasenatdpltintegration01/oracleidentitycloudservice/andrew.gregory@oracle.com <test@example.com>
2022-04-14 17:42:20 -0400

    clean

--- clean_bucket.py
+++ clean_bucket.py
@@ -6,6 +6,8 @@ import os
 
 from multiprocessing import Semaphore
 from multiprocessing import Process
+import concurrent.futures
+import threading
 
 # The Compartment OCID
 compartment_id = "ocid1.compartment.oc1..aaaaaaaasczurylkzvviqfujhtinyiycg27yelt4opnwo3rgsfu22cuehv6q"
@@ -21,10 +23,10 @@ object_storage_client = oci.object_storage.ObjectStorageClient(config)
 namespace_name = object_storage_client.get_namespace().data
 
 # Concurrency
-sema = Semaphore(10)
+sema = Semaphore(20)
 
 def deleteObject(object_storage_client,namespace_name,bucket_name,object_name ):
-    #sema.acquire()
+    print(f"{threading.get_ident()} | Delete: {object_name}")
     object_storage_client.delete_object(
         namespace_name=namespace_name,
         bucket_name=bucket_name,
@@ -35,54 +37,26 @@ def deleteObject(object_storage_client,namespace_name,bucket_name,object_name ):
 # List / clean
 list_objects_response = object_storage_client.list_objects(
     namespace_name=object_storage_client.get_namespace().data,
-    bucket_name=bucket_name,
-    # prefix="EXAMPLE-prefix-Value",
-    # start="EXAMPLE-start-Value",
-    # end="EXAMPLE-end-Value",
-    # limit=824,
-    # delimiter="EXAMPLE-delimiter-Value",
-    # fields="etag",
-    # opc_client_request_id="ocid1.test.oc1..<unique_ID>EXAMPLE-opcClientRequestId-Value",
-    # start_after="EXAMPLE-startAfter-Value"
+    bucket_name=bucket_name
 )
 
-#print(list_objects_response.data)
+# Main loop
+with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+    futures = []
 
-for obj in list_objects_response.data.objects:
-    print(f"{os.getpid()} Object Name: {obj.name}")
-    # object_storage_client.delete_object(
-    #     namespace_name=object_storage_client.get_namespace().data,
-    #     bucket_name=bucket_name,
-    #     object_name=obj.name
-    # )
-    sema.acquire()
-    process = Process(target=deleteObject, args=(
-                        object_storage_client,
-                        namespace_name, 
-                        bucket_name, 
-                        obj.name
-                        ))
-    process.start()
+    for obj in list_objects_response.data.objects:
+        print(f"{os.getpid()} Object Name: {obj.name}")
+        futures.append(executor.submit(deleteObject, object_storage_client,namespace_name,bucket_name,obj.name))
 
-while list_objects_response.data.next_start_with != None:
-    print(f"{os.getpid()} Next round {list_objects_response.data.next_start_with}")
-    list_objects_response2 = object_storage_client.list_objects(
-        namespace_name=namespace_name,
-        bucket_name=bucket_name,
-    # prefix="EXAMPLE-prefix-Value",
-        start=list_objects_response.data.next_start_with
-    # end="EXAMPLE-end-Value",
-    # limit=824,
-    # delimiter="EXAMPLE-delimiter-Value",
-    # fields="etag",
-    # opc_client_request_id="ocid1.test.oc1..<unique_ID>EXAMPLE-opcClientRequestId-Value",
-    # start_after="EXAMPLE-startAfter-Value"
-)   
-    for obj in list_objects_response2.data.objects:
-        print(f"{os.getpid()} (list_objects_response.data.next_start_with) Object Name: {obj.name}")
-        process = Process(target=deleteObject, args=(
-                            object_storage_client,
-			    namespace_name, 
-                            bucket_name, 
-                            obj.name))
-        process.start()
+
+    while list_objects_response.data.next_start_with != None:
+        print(f"{os.getpid()} Next round {list_objects_response.data.next_start_with}")
+        list_objects_response2 = object_storage_client.list_objects(
+            namespace_name=namespace_name,
+            bucket_name=bucket_name,
+            start=list_objects_response.data.next_start_with
+        )  
+
+        for obj in list_objects_response2.data.objects:
+            print(f"{os.getpid()} (list_objects_response.data.next_start_with) Object Name: {obj.name}")
+            futures.append(executor.submit(deleteObject, object_storage_client,namespace_name,bucket_name,obj.name))
