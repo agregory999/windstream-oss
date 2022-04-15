267ea092a0facb365751a443f63173e41a358c76
orasenatdpltintegration01/oracleidentitycloudservice/andrew.gregory@oracle.com <test@example.com>
2022-04-15 16:47:37 -0400

    argument works

--- oss_upload.py
+++ oss_upload.py
@@ -1,4 +1,4 @@
-import array
+# import array
 from concurrent.futures import ProcessPoolExecutor
 from itertools import repeat
 from multiprocessing.connection import wait
@@ -10,10 +10,10 @@ from oci.object_storage import UploadManager
 from oci.object_storage.transfer.constants import DEFAULT_PART_SIZE
 from pathlib import Path
 
-from multiprocessing import Semaphore
-from multiprocessing import Process
+# from multiprocessing import Semaphore
+# from multiprocessing import Process
 import argparse
-import threading
+# import threading
 
 # The root directory path, Replace with your path
 folder = Path('/Users/argregor/windstream-python/files')
@@ -41,9 +41,7 @@ def progress_callback(bytes_uploaded):
     return
     #print("{} additional bytes uploaded".format(bytes_uploaded))
 
-def uploadOSSProcess(path: str, filename: str, base_object_name: str, namespace):
-
-    data = threading.local()
+def uploadOSSProcess(path: str, filename: str, base_object_name: str, namespace, verbose: bool):
 
     # Initialize (don't like this)
     config = oci.config.from_file()
@@ -52,14 +50,16 @@ def uploadOSSProcess(path: str, filename: str, base_object_name: str, namespace)
     namespace = object_storage_client.get_namespace().data
 
     full_file_name = path + "/" + filename
-    object_name = str(base_object_name) + "/" + str(filename)
+    # If root directory object, just use filename as object name
+    # otherwise take the relative path (base_object_name) and append / filename
+    object_name = str(filename) if str(base_object_name) == "" else str(base_object_name) + "/" + str(filename)
     if verbose:
-        print(f"{os.getpid()} Path: {full_file_name} Name: {base_object_name} Size: {os.stat(full_file_name).st_size} Namespace: {namespace}")
+        print(f"{os.getpid()} Path: {full_file_name} Base Name: {base_object_name} Size: {os.stat(full_file_name).st_size} Namespace: {namespace}")
         #print(f"Object name: {object_name}")
-
+        print(f"{os.getpid()} Start uploading {full_file_name} with name {object_name}")
     if os.stat(full_file_name).st_size > mp_threshold:
-        data.start = time.time()
-
+        start = time.time()
+        
         # MP
         upload_manager.upload_file(
             namespace,
@@ -68,18 +68,18 @@ def uploadOSSProcess(path: str, filename: str, base_object_name: str, namespace)
             full_file_name,
             part_size=DEFAULT_PART_SIZE,
             progress_callback=progress_callback)
-        data.end = time.time()
+        end = time.time()
         if verbose:
-            print(f"{os.getpid()} Finished MP uploading {full_file_name} Time: {(data.end - data.start):.2f}s Size: {os.stat(full_file_name).st_size} bytes")
+            print(f"{os.getpid()} Finished MP uploading {full_file_name} Time: {(end - start):.2f}s Size: {os.stat(full_file_name).st_size} bytes")
     else:
         with open(full_file_name, "rb") as in_file:
             # Reg put  
-            data.start = time.time()
+            start = time.time()
             object_storage_client.put_object(
                 namespace, bucket_name, object_name, in_file)
-            data.end = time.time()
+            end = time.time()
             if verbose:
-                print(f"{os.getpid()} Finished uploading {full_file_name} Time: {(data.end - data.start):.2f}s Size: {os.stat(full_file_name).st_size} bytes")
+                print(f"{os.getpid()} Finished uploading {full_file_name} Time: {(end - start):.2f}s Size: {os.stat(full_file_name).st_size} bytes")
 
 if __name__ == '__main__':
 
@@ -132,13 +132,33 @@ if __name__ == '__main__':
         for (root,dirs,files) in os.walk(folder, topdown=True):
             # For each directory you get a tuple - files is a list within that tuple
             if verbose:
-                print(f"OS Walk File: {files}")
+                print(f"OS Walk File: {root} / {files}")
             p = Path(root)
-            base_object_name = os.path.basename(folder) + "/" + str(p.relative_to(folder)) if add_enclosing_folder else p.relative_to(folder)
-
+            #print(f"Path rel: {p}")
+            rel_path = "" if str(p.relative_to(folder)) == "." else p.relative_to(folder)
+            if verbose:
+                print(f"Rel to {folder} : {rel_path}")
+            # If enclosing, start with that
+            if add_enclosing_folder:
+                base_object_name = os.path.basename(folder)
+                if str(rel_path) != "":
+                    base_object_name += "/"
+                    base_object_name += str(rel_path)
+            else:
+                base_object_name = str(rel_path)
+            #base_object_name = os.path.basename(folder) if add_enclosing_folder else ""
+            # Now if we are in root, do nothing
+            # but if not in root append / and path
+            #if str(rel_path) != "":
+            #    base_object_name += "/" 
+            #    base_object_name += str(rel_path)
+            #base_object_name = os.path.basename(folder) + ("/" + str(rel_path) if str(rel_path) == "" else "" ) if add_enclosing_folder else str(rel_path)
+            # Will be prepended to file name in subprocess after map
+            if verbose:
+                print(f"Base Obj Name :  {base_object_name}")
             # This is what takes the list of all files and sends it to the Process Pool
             # results is a Future List, but should be empty
-            results = executor.map(uploadOSSProcess,repeat(root),files,repeat(base_object_name),repeat(namespace))
+            results = executor.map(uploadOSSProcess,repeat(root),files,repeat(base_object_name),repeat(namespace),repeat(verbose))
     for result in results:
         if verbose:
             print (f"Result {result}")
