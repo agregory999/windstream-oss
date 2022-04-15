70a5c77bee1f0907e5be82b6a0224434049a0e40
orasenatdpltintegration01/oracleidentitycloudservice/andrew.gregory@oracle.com <test@example.com>
2022-04-15 12:45:49 -0400

    cleanup

--- oss_upload.py
+++ oss_upload.py
@@ -35,7 +35,7 @@ add_enclosing_folder = False
 concurrency = 5
 
 # MP threashold bytes
-mp_threshold = 100000000
+mp_threshold = DEFAULT_PART_SIZE
 
 def progress_callback(bytes_uploaded):
     return
@@ -47,12 +47,13 @@ def uploadOSSProcess(path: str, filename: str, base_object_name: str, namespace)
 
     # Initialize (don't like this)
     config = oci.config.from_file()
-    data.object_storage_client = oci.object_storage.ObjectStorageClient(config)
+    object_storage_client = oci.object_storage.ObjectStorageClient(config)
     upload_manager = UploadManager(object_storage_client, allow_parallel_uploads=True)
     namespace = object_storage_client.get_namespace().data
 
     full_file_name = path + "/" + filename
-    print(f"{os.getpid()} Path: {full_file_name} Name: {base_object_name} Size: {os.stat(full_file_name).st_size} Namespace: {namespace}")
+    if verbose:
+        print(f"{os.getpid()} Path: {full_file_name} Name: {base_object_name} Size: {os.stat(full_file_name).st_size} Namespace: {namespace}")
 
     object_name = str(base_object_name) + "/" + str(filename)
     if os.stat(full_file_name).st_size > mp_threshold:
@@ -67,7 +68,8 @@ def uploadOSSProcess(path: str, filename: str, base_object_name: str, namespace)
             part_size=DEFAULT_PART_SIZE,
             progress_callback=progress_callback)
         data.end = time.time()
-        print(f"{os.getpid()} Finished MP uploading {full_file_name} Time: {(data.end - data.start):.2f}s Size: {os.stat(full_file_name).st_size} bytes")
+        if verbose:
+            print(f"{os.getpid()} Finished MP uploading {full_file_name} Time: {(data.end - data.start):.2f}s Size: {os.stat(full_file_name).st_size} bytes")
     else:
         with open(full_file_name, "rb") as in_file:
             # Reg put  
@@ -75,7 +77,8 @@ def uploadOSSProcess(path: str, filename: str, base_object_name: str, namespace)
             object_storage_client.put_object(
                 namespace, bucket_name, object_name, in_file)
             data.end = time.time()
-            print(f"{os.getpid()} Finished uploading {full_file_name} Time: {(data.end - data.start):.2f}s Size: {os.stat(full_file_name).st_size} bytes")
+            if verbose:
+                print(f"{os.getpid()} Finished uploading {full_file_name} Time: {(data.end - data.start):.2f}s Size: {os.stat(full_file_name).st_size} bytes")
 
 if __name__ == '__main__':
 
@@ -87,6 +90,7 @@ if __name__ == '__main__':
     parser.add_argument("-c", "--compartment", help="OCID of compartment")
     parser.add_argument("-p", "--parallelism", type=int, help="parallel processes allowed")
     parser.add_argument("-f", "--folder", type=Path, help="path to local folder to upload", required=True)
+    parser.add_argument("-th", "--threshold", type=int, help="threshold in bytes for multi-part upload")
     args = parser.parse_args()
 
 
@@ -111,22 +115,30 @@ if __name__ == '__main__':
     if args.parallelism:
         concurrency = args.parallelism
 
-    print (f"**** Start - using {folder} with parallelism of {concurrency}.  Bucket name={bucket_name} ***")
+    # Threshold
+    if args.threshold:
+        mp_threshold = args.threshold
+
+    print (f"**** Start - using {folder} with parallelism of {concurrency} and File threshold: {mp_threshold}.  Bucket name={bucket_name} ***")
 
     config = oci.config.from_file()
     object_storage_client = oci.object_storage.ObjectStorageClient(config)
     namespace = object_storage_client.get_namespace().data
  
     # Try with Process Pool
-    futures = []
     with ProcessPoolExecutor(concurrency) as executor:
+        # os.walk does recursive file tree walk and gives us paths 
         for (root,dirs,files) in os.walk(folder, topdown=True):
-            print(f"OS Walk Root: {root}")
-            print(f"OS Walk File: {files}")
+            # For each directory you get a tuple - files is a list within that tuple
+            if verbose:
+                print(f"OS Walk File: {files}")
             p = Path(root)
             base_object_name = os.path.basename(folder) + "/" + str(p.relative_to(folder)) if add_enclosing_folder else p.relative_to(folder)
-            #object_name = os.path.basename(folder) + "/" + object.relative_to(folder).as_posix() if add_enclosing_folder else object.relative_to(folder).as_posix()
+
+            # This is what takes the list of all files and sends it to the Process Pool
+            # results is a Future List, but should be empty
             results = executor.map(uploadOSSProcess,repeat(root),files,repeat(base_object_name),repeat(namespace))
     for result in results:
-        print(result)
+        if verbose:
+            print (f"Result {result}")
 
