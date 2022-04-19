62778ba44fc267f16431fb5feec1a28ee69e6f43
orasenatdpltintegration01/oracleidentitycloudservice/andrew.gregory@oracle.com <test@example.com>
2022-04-19 09:45:56 -0400

    clean script

--- clean_bucket.py
+++ clean_bucket.py
@@ -22,7 +22,7 @@ bucket_name = None
 
 
 def deleteObject(object_storage_client,namespace_name,bucket_name,object_name ):
-    print(f"{threading.get_ident()} | Delete: {object_name}")
+    #print(f"{threading.get_ident()} | Delete: {object_name}")
     object_storage_client.delete_object(
         namespace_name=namespace_name,
         bucket_name=bucket_name,
@@ -54,37 +54,47 @@ object_storage_client = oci.object_storage.ObjectStorageClient(config)
 namespace_name = object_storage_client.get_namespace().data
 
 
-# Initial bucket List - response element count limited by API
-list_objects_response = object_storage_client.list_objects(
-    namespace_name=object_storage_client.get_namespace().data,
-    bucket_name=bucket_name
-)
-count = 0
-start = time.time()
 
 # Main loop - delete objects, then if more, repeat on that until gone
 with concurrent.futures.ThreadPoolExecutor(max_workers=concurrency) as executor:
     futures = []
 
-    for obj in list_objects_response.data.objects:
-        print(f"{os.getpid()} Object Delete: {obj.name}")
-        futures.append(executor.submit(deleteObject, object_storage_client,namespace_name,bucket_name,obj.name))
-        count += 1
+    obj_count = 0
+    iteration = 0
+    start = time.time()
+    more_to_do = True
+    while more_to_do:
+        print(f"{os.getpid()} Iteration: {iteration}")
+        # Initial bucket List - response element count limited by API
+        list_objects_response = object_storage_client.list_objects(
+            namespace_name=object_storage_client.get_namespace().data,
+            bucket_name=bucket_name
+        )
 
-    while list_objects_response.data.next_start_with != None:
-        print(f"{os.getpid()} Performing another iteration of List/delete: {list_objects_response.data.next_start_with}")
-        list_objects_response2 = object_storage_client.list_objects(
-            namespace_name=namespace_name,
-            bucket_name=bucket_name,
-            start=list_objects_response.data.next_start_with
-        )  
-
-        for obj in list_objects_response2.data.objects:
-            print(f"{os.getpid()} Object Delete: {obj.name}")
+        for obj in list_objects_response.data.objects:
+            if verbose:
+                print(f"{os.getpid()} Iter: {iteration} | Object Delete: {obj.name}")
             futures.append(executor.submit(deleteObject, object_storage_client,namespace_name,bucket_name,obj.name))
-            count += 1
-end = time.time()
+            obj_count += 1
+        iteration += 1
+        # Ternary - to control loop
+        more_to_do = True if list_objects_response.data.next_start_with != None else False
+        print(f"Control data: {list_objects_response.data.next_start_with} | Var: {more_to_do}")
+        # while list_objects_response.data.next_start_with != None:
+        #     print(f"{os.getpid()} Performing another iteration of List/delete: {list_objects_response.data.next_start_with}")
+        #     list_objects_response2 = object_storage_client.list_objects(
+        #         namespace_name=namespace_name,
+        #         bucket_name=bucket_name,
+        #         start=list_objects_response.data.next_start_with
+        #     )  
+
+        #     for obj in list_objects_response2.data.objects:
+        #         print(f"{os.getpid()} Object Delete: {obj.name}")
+        #         futures.append(executor.submit(deleteObject, object_storage_client,namespace_name,bucket_name,obj.name))
+        #         count += 1
+    end = time.time()
+for result in futures:
+    if verbose:
+        print (f"Result {result}")
 if verbose:
-    print(f"{os.getpid()} Count of deleted objects: {count}  | Time taken: {(end - start):.2f}s")  
-concurrent.futures.wait(futures)
-executor.shutdown()
+    print(f"{os.getpid()} Count of deleted objects: {obj_count}  | Time taken: {(end - start):.2f}s",flush=True)  
