e114147fddf1bbfd84ba4f8e0bd5885aeb99cc1f
Andrew Gregory <andrew.gregory@oracle.com>
2022-08-07 08:32:11 -0400

    verbose

--- fss_backup.py
+++ fss_backup.py
@@ -203,24 +203,26 @@ for share in shares.data:
         print(f"Using Remote Path (rclone_remote:bucket/snapshot): {rclone_remote}{backup_bucket_name}/{snapshot_name}")
 
     # Call out to rclone it
+    # Additional flags to consider
+    # --s3-disable-checksum   only for large objects, avoid md5sum which is slow
     if not dry_run:
         if verbose:
-            print(f"Calling rclone with rclone sync -v --metadata --max-backlog 999999 --links --s3-disable-checksum --s3-chunk-size=16M --s3-upload-concurrency={core_count} --transfers={core_count} --checkers={core_count*2} /mnt/temp-backup/.snapshot/{snapshot_name} {remote_path}", flush=True)
+            print(f"Calling rclone with rclone sync -v --metadata --max-backlog 999999 --links --s3-chunk-size=16M --s3-upload-concurrency={core_count} --transfers={core_count} --checkers={core_count*2} /mnt/temp-backup/.snapshot/{snapshot_name} {remote_path}", flush=True)
         
         # Try / catch so as to not kill the process
         try:
-            completed = subprocess.run(["rclone","sync", "-v", "--metadata", "--max-backlog", "999999", "--links", "--s3-disable-checksum", "--s3-chunk-size=16M", f"--s3-upload-concurrency={core_count}", f"--transfers={core_count}",f"--checkers={core_count*2}",f"/mnt/temp-backup/.snapshot/{snapshot_name}",f"{remote_path}"],shell=False, check=True)
+            completed = subprocess.run(["rclone","sync", "-v", "--metadata", "--max-backlog", "999999", "--links",  "--s3-chunk-size=16M", f"--s3-upload-concurrency={core_count}", f"--transfers={core_count}",f"--checkers={core_count*2}",f"/mnt/temp-backup/.snapshot/{snapshot_name}",f"{remote_path}"],shell=False, check=True)
             print (f"RCLONE output: {completed.stdout}")
         except subprocess.CalledProcessError:
             print(f"RCLONE ERROR: Continue processing")
 
         if backup_type in ['weekly','monthly']:
             if verbose:
-                print(f"Creating additional {backup_type} backup called {additional_snapshot_name}. Implemented as rclone copy")
+                print(f"Creating additional {backup_type} backup called {additional_snapshot_name}. Implemented as rclone server side copy")
                 print(f"Calling rclone with rclone sync -v --transfers={core_count} --checkers={core_count*2} {remote_path} {additional_remote_path}", flush=True)
             # Try / catch so as to not kill the process
             try:
-                completed = subprocess.run(["rclone","copy", "-v", f"--transfers={core_count}",f"--checkers={core_count*2}",f"{remote_path}", f"{additional_remote_path}"],shell=False, check=True)
+                completed = subprocess.run(["rclone","copy", f'{"-vv" if verbose else "-v"}', f"--transfers={core_count}",f"--checkers={core_count*2}",f"{remote_path}", f"{additional_remote_path}"],shell=False, check=True)
                 print (f"RCLONE output: {completed.stdout}")
             except subprocess.CalledProcessError:
                 print(f"RCLONE ERROR: Continue processing")
